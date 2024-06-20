8becf14ccb3ddf448e453fdb1af25db774e18089
Alex Reis <alexreiis1984@gmail.com>
2024-06-20 15:01:38 -0300

    feature: Finalização do conteudo da rota personal_data

--- usuarios/src/index.js
+++ usuarios/src/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const router = require("./routes/users/users")
+const router = require("./routes/users/users.js")
 const app = express();
+const router_personal = require("./routes/personaldata/personal_data.js");
 app.use(express.json());
 app.use("/api/v1/users/",router);
+app.use("/api/v1/personaldata/",router_personal);
 app.listen(process.env.HOST_PORT,()=>console.log(`Servidor online em: ${process.env.HOST_NAME}:${process.env.HOST_PORT}`));
--- usuarios/src/routes/personaldata/personal_data.js
+++ usuarios/src/routes/personaldata/personal_data.js
@@ -1,9 +1,10 @@
 require("dotenv").config()
 const express = require("express")
-const router_personal = express.router()
+const router_personal = express.Router()
 const data= require("../../database/config.js")
+const verifica = require("../../middleware/verify_token.js")
 
-router_personal.get("/listar",(req,res)=>{
+router_personal.get("/listar",verifica,(req,res)=>{
     data.query("select * from dadospessoais",(Error,result)=>{
         if(Error){
             return res.status(500).send({msg:"Erro ao carregar os dados"})
@@ -12,7 +13,7 @@ router_personal.get("/listar",(req,res)=>{
     })
 })
 
-router_personal.get("/listar/:cpf",(req,res)=>{
+router_personal.get("/listar/:cpf",verifica,(req,res)=>{
     data.query("select * from dadospessoais where iddadospessoais=?",req.params.cpf,(Error,result)=>{
         if(Error){
             return res.status(500).send({msg:"Erro ao carregar os dados"})
@@ -20,7 +21,7 @@ router_personal.get("/listar/:cpf",(req,res)=>{
         res.status(200).send({msg:"OK",payload:result})
     })
 })
-router_personal.post("/cadastar",(req,res)=>{
+router_personal.post("/cadastrar",verifica,(req,res)=>{
     data.query("insert into dadospessoais set ?",req.body,(Error,result)=>{
         if(Error){
             return res.status(500).send({msg:"Erro ao tentar cadastrar"})
@@ -28,3 +29,12 @@ router_personal.post("/cadastar",(req,res)=>{
         res.status(201).send({msg:"OK",payload:result})
     })
 })
+router_personal.put("/atualizar/:id",verifica,(req, res)=> {
+    data.query("update dadospessoais set ? where iddadospessoais=?",[req.body,req.params.id],(Error, result)=>{
+        if (Error){
+            return res.status(500).send({msg:"Erro ao tentar atualizar os dados"})
+        }
+        res.status(200).send({msg:"OK",payload:result})
+        })
+})
+module.exports = router_personal;
